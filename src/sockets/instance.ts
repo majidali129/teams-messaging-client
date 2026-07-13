a970f0b13671b2e44017db31d1022bceed926960
Majid Ali <majidaliofficial129@gmail.com>
2026-07-13 22:39:38 +0500

    changed autoconnect to true

--- src/sockets/instance.ts
+++ src/sockets/instance.ts
@@ -1,22 +1,22 @@
-import {io, Socket} from 'socket.io-client'
+import { io, Socket } from "socket.io-client";
 
-let socket: Socket|null = null;
+let socket: Socket | null = null;
 export const socketInstance = () => {
-    if(socket) return socket;
+  if (socket) return socket;
 
-    socket = io(`${import.meta.env.VITE_API_URL}/chats`, {
-        auth: {
-            token: localStorage.getItem('access-token')
-        },
-        autoConnect: false,
-        transports: ["websocket"]
-    });
+  socket = io(`${import.meta.env.VITE_API_URL}/chats`, {
+    auth: {
+      token: localStorage.getItem("access-token"),
+    },
+    autoConnect: true,
+    transports: ["websocket"],
+  });
 
-    return socket;
-}
+  return socket;
+};
 
 export const disconnectSocket = () => {
-    if(!socket) return;
-    socket.disconnect()
-    socket = null;
-}
+  if (!socket) return;
+  socket.disconnect();
+  socket = null;
+};

012ef9027c54d45640044f826d4be4ca4ac6e55d
Majid Ali <majidaliofficial129@gmail.com>
2026-07-13 19:08:33 +0500

    fixed auth user flow

--- src/components/shared/protect-route.tsx
+++ src/components/shared/protect-route.tsx
@@ -1,21 +1,24 @@
 import { useUser } from "@/features/auth/hooks/use-user";
-import { useEffect } from "react"
-import { Navigate,  } from "react-router";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { signInPath } from "@/paths";
-
+import { LoadingState } from "./loading-state";
 
 export const ProtectRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isAuthenticated } = useUser();
+  const { isAuthenticated, isLoading } = useUser();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      <Navigate to={signInPath()} replace />;
+    if (!isAuthenticated && !isLoading) {
+      navigate(signInPath());
     }
-  }, [isAuthenticated]);
+  }, [isAuthenticated, isLoading, navigate]);
 
-  return (
-    <>
-      {children}
-    </>
-  )
-}
+  if (isLoading)
+    <LoadingState
+      title="Checking your account..."
+      description="Please wait while we verify your credentials."
+    />;
+
+  if (isAuthenticated) return children;
+};
